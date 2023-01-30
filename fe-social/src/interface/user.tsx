b3e5faf66e499c80ca51aa528f555834d40b1c4f
CanarioTOMAS <tomimartine.34@gmail.co>
2023-01-30 17:14:27 -0300

    work in progress

---
+++ fe-social/src/interface/user.tsx
@@ -0,0 +1,8 @@
+export interface IUser{
+    name:string;
+    address:string;
+    email:string;
+    phone:string;
+    password:string;
+    confirmPassword:string;
+}
--- fe-social/src/pages/formLogin/formLogin.tsx
+++ fe-social/src/pages/formLogin/formLogin.tsx
@@ -1,62 +1,84 @@
-import React, { useState } from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import style from './styleFormLogin.module.css'
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import style from "./styleFormLogin.module.css";
+import { IUser } from "../../interface/user";
+import { red } from "@mui/material/colors";
 
-
-export const FormLogin = () => {  
+export const FormLogin = () => {
   const [showPassword, setShowPassword] = React.useState(false);
-  const [formValue, setForm] = useState({
-    email: '',
-    password: ''
-  })
+  const [formValue, setForm] = useState<IUser>({
+    name: "",
+    phone: "",
+    address: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  useEffect(() => {
+    document.title = `You clicked ${formValue.email} times`;
+  });
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const handleMouseDownPassword = (event:any) => {
+  const handleMouseDownPassword = (event: any) => {
     event.preventDefault();
   };
-  const handleInputChange = (event:any) => {
-      setForm({
-         ...formValue,
-            [event.target.name] : event.target.value
-        })
-    }
-  const submit =(e:any)=>{
-    e.preventDefault()
-   console.log( formValue)
-}  
+  const handleInputChange = (event: any) => {
+    setForm({
+      ...formValue,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const submit = (e: any) => {
+    e.preventDefault();
 
+    console.log(formValue);
+  };
 
   return (
-
-    <div className={style.Body}> 
-    <div><img src="https://socialup.com.ar/wp-content/uploads/2021/11/logo-blanco-135x71.png"  alt=""/></div>
-      <FormControl sx={{ m:1, width:'25ch' }} variant="standard">
-        <TextField label="Email" variant="standard" type='email' name='email' required value={formValue.email} onChange={handleInputChange}></TextField>
-      </FormControl>
-    <Box>
-      <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
-          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-          <Input
-          value={formValue.password} 
-          name="password"
+    <div className={style.Body}>
+      <div>
+        <img
+          src="https://socialup.com.ar/wp-content/uploads/2021/11/logo-blanco-135x71.png"
+          alt=""
+        />
+      </div>
+      <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
+        <TextField
+          label="Email"
+          variant="standard"
+          type="email"
+          name="email"
+          required
+          value={formValue.email}
           onChange={handleInputChange}
+        ></TextField>
+      </FormControl>
+      <Box>
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">
+            Password
+          </InputLabel>
+          <Input
+            value={formValue.password}
+            name="password"
+            onChange={handleInputChange}
             id="standard-adornment-password"
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -70,28 +92,27 @@ export const FormLogin = () => {
             }
           />
         </FormControl>
-        </Box>
-        <Box>
-        <Button variant="contained" endIcon={<SendIcon />} onClick={submit} >
-            Sign In
-</Button>
-</Box>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Grid>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-                </Grid> 
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-
-
+      </Box>
+      <Box>
+        <Button variant="contained" endIcon={<SendIcon />} onClick={submit}>
+          Sign In
+        </Button>
+      </Box>
+{formValue.email&& formValue.password!=="" && <FormControlLabel
+        control={<Checkbox value="remember" color="primary" />}
+        label="Remember me"
+      />}
+      <Grid>
+        <Link className={style.forgotPassword} href="/forgotPassword" variant="body2" color="#6b0040">
+          Forgot password?
+        </Link>
+      </Grid>
+      <Grid item>
+      <Link href="/register" variant="body2" underline="none" color="#6b0040">Don't have an account?</Link>
+        <Link href="/register" variant="subtitle1" color="#92213c" underline="hover" >
+          { "Sign Up"}
+        </Link>
+      </Grid>
     </div>
-  )
-}
+  );
+};
