5b8ae899ca0a9f38f1847ebcdd431504771c5fb3
Lucas Gutierrez <luquitas_17@hotmail.com>
2023-02-01 15:47:18 -0300

    work in progress

--- fe-social/src/pages/formRegister/formRegister.tsx
+++ fe-social/src/pages/formRegister/formRegister.tsx
@@ -10,9 +10,30 @@ import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import React from 'react';
+import React, { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import style from './styleFormRegister.module.css'
+import { Card } from '@mui/material';
 
 export const FormRegister = () => {
+
+    const [formValue, setFormValue] = useState({
+      name: "",
+      phone: "",
+      homeAdress: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    }) 
+
+    const handleInputChange = (event: any ) => {
+      setFormValue ({
+        ...formValue,
+        [event.target.name]: event.target.value
+      })
+    }
+
     const [showPassword, setShowPassword] = React.useState(false);
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -20,29 +41,23 @@ export const FormRegister = () => {
     const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
       event.preventDefault();
     }
-
+    
+    const submit = (event: any ) => {
+      event.preventDefault()
+      console.log(formValue)
+    }
     return (
       <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+      <Card sx={{p:1}}>
       <div>
-        <TextField
-          label="Name"
-          sx={{ m: 1, width: '25ch' }}
-        />
-        <TextField
-          label="Phone"
-          sx={{ m: 1, width: '25ch' }}
-        />
-        <TextField
-          label="Home address"
-          sx={{ m: 1, width: '25ch' }}
-        />
-        <TextField
-          label="Email"
-          sx={{ m: 1, width: '25ch' }}
-        />
-        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+        <Box><TextField label="Name" sx={{ m: 1, width: '25ch' }} type="text" onChange={handleInputChange} name= "name" value={formValue.name}/></Box>
+        <Box><TextField label="Phone" sx={{ m: 1, width: '25ch' }} type="tel" onChange={handleInputChange} name= "phone" value={formValue.phone}/></Box>
+        <Box><TextField label="Home address" sx={{ m: 1, width: '25ch' }} type="text" onChange={handleInputChange} name= "homeAdress" value={formValue.homeAdress}/></Box>
+        <Box><TextField label="Email" sx={{ m: 1, width: '25ch' }} type="email" onChange={handleInputChange} name= "email" value={formValue.email}/></Box>
+        <Box><FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
           <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
           <OutlinedInput
+            onChange={handleInputChange} name= "password" value={formValue.password}
             type={showPassword ? 'text' : 'password'}
             endAdornment={
               <InputAdornment position="end">
@@ -55,12 +70,12 @@ export const FormRegister = () => {
                 </IconButton>
               </InputAdornment>
             }
-            label="Password"
-          />
-        </FormControl>
-        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            label="Password"/>
+        </FormControl></Box>
+        <Box><FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
           <InputLabel htmlFor="outlined-adornment-password">Confirm password</InputLabel>
           <OutlinedInput
+            onChange={handleInputChange} name= "confirmPassword" value={formValue.confirmPassword}
             type={showPassword ? 'text' : 'password'}
             endAdornment={
               <InputAdornment position="end">
@@ -73,10 +88,15 @@ export const FormRegister = () => {
                 </IconButton>
               </InputAdornment>
             }
-            label="Confirm password"
-          />
-        </FormControl>
-        </div>
+            label="Confirm password"/>
+        </FormControl></Box>
+        <Box>
+          <Button onClick={submit} 
+          className = {style.submit} variant="contained">Submit</Button>
+        </Box>
+      </div>
+      </Card>
+      
       </Box>
     );
 }
